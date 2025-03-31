155d2a94e0825b4bef0f21e65e78cf5bf13e4681
Tony Lin <99093620+justapotato213@users.noreply.github.com>
2025-03-31 19:53:15 -0400

    add ads1115 to startup code

--- src/rp2040_bringup.c
+++ src/rp2040_bringup.c
@@ -40,13 +40,13 @@
 #endif /* CONFIG_ARCH_BOARD_COMMON */
 
 #ifdef CONFIG_ADC_ADS1115
-#include <nuttx/analog/ads1115.h>
 #include "rp2040_i2c.h"
 #include <nuttx/analog/adc.h>
+#include <nuttx/analog/ads1115.h>
 #endif
 
 #ifdef CONFIG_USERLED
-#  include <nuttx/leds/userled.h>
+#include <nuttx/leds/userled.h>
 #endif
 
 /****************************************************************************
@@ -69,22 +69,33 @@ int rp2040_bringup(void)
 
 #endif /* CONFIG_ARCH_BOARD_COMMON */
 
-  /* --- Place any board specific bringup code here --- */
+    /* --- Place any board specific bringup code here --- */
 
 #ifdef CONFIG_ADC_ADS1115
+  int ads1115_addrs[3] = {0x48, 0x49, 0x4A};
+
   /* Register the ADS1115 ADC driver */
-  struct adc_dev_s *ads1115 = ads1115_initialize(rp2040_i2cbus_initialize(0),
-                                                 CONFIG_ADC_ADS1115_ADDR);
-  if (ads1115 == NULL)
+
+  for (int i = 0; i < 3; i++)
     {
-      syslog(LOG_ERR, "Failed to initialize ADS1115\n");
+      struct adc_dev_s *ads1115 =
+          ads1115_initialize(rp2040_i2cbus_initialize(0), ads1115_addrs[i]);
+      if (ads1115 == NULL)
+        {
+          syslog(LOG_ERR, "Failed to initialize ADS1115\n");
+        }
+
+      char devpath[] = "/dev/adcx";
+      devpath[8] = i;
+
+      ret = adc_register(devpath, ads1115);
+      if (ret < 0)
+        {
+          syslog(LOG_ERR, "Failed to register ADS1115 device driver: %d\n",
+                 ret);
+        }
     }
 
-  ret = adc_register("/dev/adc1", ads1115);
-  if (ret < 0)
-    {
-      syslog(LOG_ERR, "Failed to register ADS1115 device driver: %d\n", ret);
-    }
 #endif
 
 #ifdef CONFIG_USERLED
@@ -93,8 +104,7 @@ int rp2040_bringup(void)
   ret = userled_lower_initialize("/dev/userleds");
   if (ret < 0)
     {
-      syslog(LOG_ERR, \
-      "ERROR: userled_lower_initialize() failed: %d\n", ret);
+      syslog(LOG_ERR, "ERROR: userled_lower_initialize() failed: %d\n", ret);
     }
 #endif
 
